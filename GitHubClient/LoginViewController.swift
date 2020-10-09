163c4bc965ccf224735611dc68190318887a390a
Pavel Laukhin <6886834@gmail.com>
2020-10-09 12:49:23 +0300

    Rounded SignIn button

--- GitHubClient/LoginViewController.swift
+++ GitHubClient/LoginViewController.swift
@@ -11,18 +11,24 @@ import Kingfisher
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var logoView: UIImageView!
+    @IBOutlet weak var signInButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "GitHub.com"
         addGitHubLogo()
+        makeButtonRounded()
     }
 
     private func addGitHubLogo() {
         // Установим логотип GitHub:
         let url = URL(string: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")
         logoView.kf.setImage(with: url)
+    }
+    
+    private func makeButtonRounded() {
+        signInButton.layer.cornerRadius = 6
     }
 
 }
