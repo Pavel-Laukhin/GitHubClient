cc03e61897dd1873f40a3b64270b31c3225e2cdd
Pavel Laukhin <6886834@gmail.com>
2020-10-09 20:32:57 +0300

    1. URLRequest handler added. 2. Avatar image has been made rounded. 3. Added possibility to choose the number of stars for adding into request.

--- GitHubClient/HelloViewController.swift
+++ GitHubClient/HelloViewController.swift
@@ -19,6 +19,7 @@ class HelloViewController: UIViewController {
     private let avatarView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .systemTeal
         return imageView
     }()
     private let searchLabel: UILabel = {
@@ -45,11 +46,34 @@ class HelloViewController: UIViewController {
         textField.layer.cornerRadius = 5
         return textField
     }()
+    private let switcher: UISwitch = {
+        let switcher = UISwitch()
+        switcher.isOn = false
+        switcher.addTarget(self, action: #selector(switchHandler), for: .valueChanged)
+        return switcher
+    }()
+    private let starsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Stars >"
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.isEnabled = false
+        return label
+    }()
+    private let starsNumberTextField: UITextField = {
+        let textField = UITextField()
+        textField.font = UIFont.systemFont(ofSize: 17)
+        textField.placeholder = "0"
+        textField.borderStyle = .roundedRect
+        textField.layer.borderColor = UIColor.systemGray.cgColor
+        textField.layer.cornerRadius = 5
+        textField.keyboardType = .numberPad
+        textField.isEnabled = false
+        return textField
+    }()
     private let segmentedControl: UISegmentedControl = {
         let items = ["ascended", "descended"]
         let sc = UISegmentedControl(items: items)
         sc.layer.cornerRadius = 5
-        sc.addTarget(self, action: #selector(segmentedControlHandler(sender:)), for: .valueChanged)
         sc.selectedSegmentIndex = 0
         return sc
     }()
@@ -85,6 +109,9 @@ class HelloViewController: UIViewController {
          searchLabel,
          repositoryNameTextField,
          languageTextField,
+         switcher,
+         starsLabel,
+         starsNumberTextField,
          segmentedControl,
          startSearchButton
         ].forEach {
@@ -94,14 +121,16 @@ class HelloViewController: UIViewController {
     }
     
     private func setupSubviews() {
+        let avatarWidth = view.bounds.width / 3
+        
         NSLayoutConstraint.activate([
             helloLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
             helloLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
             avatarView.topAnchor.constraint(equalTo: helloLabel.bottomAnchor, constant: 20),
             avatarView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            avatarView.widthAnchor.constraint(equalToConstant: view.bounds.width / 3),
-            avatarView.heightAnchor.constraint(equalToConstant: view.bounds.width / 3),
+            avatarView.widthAnchor.constraint(equalToConstant: avatarWidth),
+            avatarView.heightAnchor.constraint(equalToConstant: avatarWidth),
             
             searchLabel.topAnchor.constraint(equalTo: avatarView.bottomAnchor, constant: 40),
             searchLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -114,25 +143,63 @@ class HelloViewController: UIViewController {
             languageTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             languageTextField.widthAnchor.constraint(equalToConstant: view.bounds.width - 2 * 40),
             
-            segmentedControl.topAnchor.constraint(equalTo: languageTextField.bottomAnchor, constant: 20),
+            switcher.leadingAnchor.constraint(equalTo: languageTextField.leadingAnchor),
+            switcher.centerYAnchor.constraint(equalTo: starsNumberTextField.centerYAnchor),
+            
+            starsLabel.leadingAnchor.constraint(equalTo: switcher.trailingAnchor, constant: 15),
+            starsLabel.centerYAnchor.constraint(equalTo: starsNumberTextField.centerYAnchor),
+
+            
+            starsNumberTextField.topAnchor.constraint(equalTo: languageTextField.bottomAnchor, constant: 15),
+            starsNumberTextField.leadingAnchor.constraint(equalTo: starsLabel.trailingAnchor, constant: 15),
+            
+            segmentedControl.topAnchor.constraint(equalTo: starsNumberTextField.bottomAnchor, constant: 20),
             segmentedControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             segmentedControl.widthAnchor.constraint(equalToConstant: view.bounds.width - 2 * 40),
             
             startSearchButton.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 40),
             startSearchButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         ])
+        avatarView.layer.cornerRadius = avatarWidth / 2
     }
     
-    @objc private func segmentedControlHandler(sender: UISegmentedControl) {
-        
+    @objc private func switchHandler() {
+        switch switcher.isOn {
+        case true:
+            starsLabel.isEnabled = true
+            starsNumberTextField.isEnabled = true
+        case false:
+            starsLabel.isEnabled = false
+            starsNumberTextField.isEnabled = false
+        }
     }
     
     @objc private func startSearchButtonPressed() {
-        
+        guard let searchString = repositoryNameTextField.text,
+              let language = languageTextField.text
+        else {
+            return
+        }
+        let stars = switcher.isOn ? Int(starsNumberTextField.text ?? "0") : nil
+        let queryEngine = QueryEngine(searchString: searchString, language: language, stars: stars)
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            queryEngine.order = "asc"
+        case 1:
+            queryEngine.order = "desc"
+        default:
+            fatalError("Index of segmented control is out of range!")
+        }
+        queryEngine.performSearchRepoRequest()
     }
     
     private func turnOffAutoResisingMask(_ view: UIView) {
         view.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    // Метод, который убирает клавиатуру после того, как закончилось редактирование
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
 }
