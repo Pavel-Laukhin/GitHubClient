4b5787c925a09c79116b2acd43043b17e6ef1711
Pavel Laukhin <6886834@gmail.com>
2020-10-09 12:45:12 +0300

    GitHub logo added

--- GitHubClient/LoginViewController.swift
+++ GitHubClient/LoginViewController.swift
@@ -14,9 +14,16 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "GitHub.com"
+        addGitHubLogo()
     }
 
+    private func addGitHubLogo() {
+        // Установим логотип GitHub:
+        let url = URL(string: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")
+        logoView.kf.setImage(with: url)
+    }
 
 }
 
